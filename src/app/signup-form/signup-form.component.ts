ce0425a270b8ef758a79a42a316d4eecb6b49b67
Saqib Naveed <saqibnaveed27@gmail.com>
2018-01-22 23:49:43 +0500

    Update reactive form to contain nested formGroups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -10,22 +10,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SignupFormComponent {
 
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace
-    ],
-    UsernameValidators.shouldBeUnique
-  ),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace
+      ],
+      UsernameValidators.shouldBeUnique
+    ),
+      password: new FormControl('', Validators.required)
+    })
   });
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 
   get password () {
-    return this.form.get('password');
+    return this.form.get('account.password');
   }
 
   login() {
