66f16cfd12020601a3e5765745c5197ef98efa1f
Saqib Naveed <saqibnaveed27@gmail.com>
2018-01-25 23:46:10 +0500

    Add http crud operations to post component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -9,15 +9,46 @@ import { Http } from '@angular/http';
 export class PostComponent implements OnInit {
 
   posts: any[];
+  private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: Http) {
-    http.get('http://jsonplaceholder.typicode.com/posts')
-      .subscribe((response) => {
-        this.posts = response.json();
-      });
+  constructor(private http: Http) {
    }
 
   ngOnInit() {
+    this.http.get(this.url)
+    .subscribe((response) => {
+      this.posts = response.json();
+    });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = {
+      title: input.value
+    };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+        // console.log(response.json());
+      });
+  }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response.json());
+      });
+    // this.http.put(this.url, JSON.stringify(post))
+  }
+
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+        // console.log(response.json());
+      });
   }
 
 }
